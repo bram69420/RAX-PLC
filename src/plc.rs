75a39dae4f57edba3c6a0d22ce07e762ddeac75e
hwollwage <hanzelputrawollwage@gmail.com>
2026-08-28 10:23:17 +0700

    batak tidur besok sepong israel lagi

--- src/plc.rs
+++ src/plc.rs
@@ -2,69 +2,40 @@ use crate::error::PlcError;
 use crate::ffi;
 
 pub struct Plc {
-    initialized: bool,
+    // no flag state, lifetime struct dah ckup
 }
 
 impl Plc {
     pub fn connect() -> Result<Self, PlcError> {
         let result = unsafe { ffi::plc_init() };
-
         if result != 0 {
             return Err(PlcError::from(result));
         }
-
-        Ok(Self {
-            initialized: true,
-        })
+        Ok(Self {})
     }
 
     pub fn read_u32(&self, address: u32) -> Result<u32, PlcError> {
-        if !self.initialized {
-            return Err(PlcError::NotInitialized);
-        }
-
         let mut value = 0u32;
-
-        let result = unsafe {
-            ffi::plc_read_u32(address, &mut value)
-        };
-
+        let result = unsafe { ffi::plc_read_u32(address, &mut value) };
         if result != 0 {
             return Err(PlcError::from(result));
         }
-
         Ok(value)
     }
 
-    pub fn write_u32(
-        &self,
-        address: u32,
-        value: u32,
-    ) -> Result<(), PlcError> {
-        if !self.initialized {
-            return Err(PlcError::NotInitialized);
-        }
-
-        let result = unsafe {
-            ffi::plc_write_u32(address, value)
-        };
-
+    pub fn write_u32(&self, address: u32, value: u32) -> Result<(), PlcError> {
+        let result = unsafe { ffi::plc_write_u32(address, value) };
         if result != 0 {
             return Err(PlcError::from(result));
         }
-
         Ok(())
     }
 }
 
 impl Drop for Plc {
     fn drop(&mut self) {
-        if self.initialized {
-            unsafe {
-                let _ = ffi::plc_shutdown();
-            }
-
-            self.initialized = false;
+        unsafe {
+            let _ = ffi::plc_shutdown();
         }
     }
 }
